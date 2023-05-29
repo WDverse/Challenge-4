9d622d4c9973dc3983de79231b9b50a3232bdbc1
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-05-28 22:13:37 -0400

    updated html and js files

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+var startQuizEl = document.querySelector("#start-quiz");
+var timerEl = document.querySelector("#timer");
+var timer = "";
+var finalScore = 0;
+var buttonEl = document.getElementById("buttons");
+var questionEl = document.querySelector ("#question");
+
+function beginQuiz (){
+    timerEl.textContent = "Time Left: " + timer;
+}
+
+
+startQuizEl.addEventListener("click", function(){
+    timer.textContent = "Time Left: " + 75;
+    questionEl.textContent = "Which of the following is not a primitive data set?";
+    var button1 = document.createElement("button");
+    var button2 = document.createElement("button");
+    var button3 = document.createElement("button");
+    var button4 = document.createElement("button");
+
+    button1.textContent = "string";
+    button2.textContent = "boolean";
+    button3.textContent = "number";
+    button4.textContent = "object";
+
+    buttonEl.appendChild(button1)
+    buttonEl.appendChild(button2)
+    buttonEl.appendChild(button3)
+    buttonEl.appendChild(button4)
+} )
+
+
